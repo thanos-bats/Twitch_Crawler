39d123c1d679280a0271324c847ca9eb05fce155
thanos-bats <thanos.batsiou@gmail.com>
2023-06-26 12:45:20 +0300

    Add scheduler to add games from the twitch api

--- Twitch_API/main.py
+++ Twitch_API/main.py
@@ -1,6 +1,24 @@
 from app import create_app, socketio
+from apscheduler.schedulers.background import BackgroundScheduler
+from services.games import get_games
+import threading
+
 
 app = create_app()
 
-print('> Starting socketio.run(app)')
-socketio.run(app, debug=False, port=3000, host="0.0.0.0")
+scheduler = BackgroundScheduler()
+
+def run_scheduler():
+    scheduler.add_job(func=get_games, args=(None, {"data": []}), trigger='interval', hours=1)
+    print('Scheduler started')
+    scheduler.start()
+
+def run_app():
+    print('> Starting socketio.run(app)')
+    socketio.run(app, debug=False, port=3000, host="0.0.0.0")
+
+if __name__ == '__main__':
+    scheduler_thread = threading.Thread(target=run_scheduler)
+    scheduler_thread.start()
+
+    run_app()
