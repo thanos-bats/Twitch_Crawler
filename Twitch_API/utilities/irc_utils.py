22d617da2de3287a6d5cdafa433fdcac836348cb
thanos-bats <thanos.batsiou@gmail.com>
2023-06-27 10:48:38 +0300

    Handle and modify multiple messages

--- Twitch_API/utilities/irc_utils.py
+++ Twitch_API/utilities/irc_utils.py
@@ -2,6 +2,7 @@ import threading
 from datetime import timezone
 import datetime
 from app import socketio
+import re
 
 def send_command(irc, command):
     print(f'< {command}')
@@ -17,22 +18,26 @@ def handle_messages(irc):
         except Exception as e:
             break
 
-        if not data:
-            continue
-        elif "PING" in data:
-            print("PING - PONG")
-            send_command(irc, "PONG tmi.twitch.tv")
-        elif "JOIN" in data:
-            print("JOIN - successfully joined")
-            print(f'> {data}')
-        elif "PRIVMSG" in data:
-            socketio.emit('message', modify_message(data))
-        else:
-            print("SOMETHINGELSE: "+data)
+        for msg in data.strip().split("\n"):
+            if "PING" in msg:
+                print("PING - PONG")
+                send_command(irc, "PONG tmi.twitch.tv")
+            elif "JOIN" in msg:
+                print("JOIN - successfully joined")
+                print(f'> {msg}')
+            elif "PRIVMSG" in msg:
+                try:
+                    modified_message = modify_message(msg)
+                    socketio.emit('message', modified_message)
+                except Exception as e:
+                    print(f"Error: {e}")
+            else:
+                print(">>: "+ msg)
         
         stop_flag = getattr(threading.current_thread(), "stop_flag", False)
 
 def modify_message(data):
+    '''
     data = data.split(':')
     message = data[2].strip()
 
@@ -42,6 +47,24 @@ def modify_message(data):
 
     data = {
         "streamer":channel, 
+        "username": username,
+        "message": message,
+        "created_at": datetime.datetime.now(timezone.utc).strftime("%Y-%m-%d %H:%M:%S") 
+    }
+
+    return data'''
+    pattern = r":(?P<username>[^!]+)![^#]+#(?P<streamer>[^\s]+)\s*:(?P<message>.*)"
+    match = re.match(pattern, data)
+    
+    if not match:
+        return None
+
+    username = match.group('username')
+    streamer = match.group('streamer')
+    message = match.group('message').strip()
+
+    data = {
+        "streamer": streamer, 
         "username": username,
         "message": message,
         "created_at": datetime.datetime.now(timezone.utc).strftime("%Y-%m-%d %H:%M:%S") 
