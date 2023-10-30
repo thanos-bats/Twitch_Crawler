45c647846993211f3d8be9c346426f72df1bb45b
thanos.batsiou <thanos.batsiou@gmail.com>
2023-10-30 11:04:31 +0200

    Print the date time of the scheduler

--- Twitch_API/main.py
+++ Twitch_API/main.py
@@ -2,6 +2,7 @@ from app import create_app, socketio
 from apscheduler.schedulers.background import BackgroundScheduler
 from services.games import get_games
 import threading
+import datetime
 
 app = create_app()
 scheduler = BackgroundScheduler()
@@ -10,12 +11,12 @@ def run_scheduler():
      with app.app_context():
         get_games(None, {"data": []})
         scheduler.add_job(func=get_games, args=(None, {"data": []}), trigger='interval', hours=1)
-        print('Scheduler started')
+        print('Scheduler started at ', datetime.datetime.now())
         scheduler.start()
 
 def run_app():
-    print('> Starting socketio.run(app)')
-    socketio.run(app, port=3000, host="0.0.0.0", use_reloader=False, allow_unsafe_werkzeug=True)
+    print('> Starting socketio.run(app)')  
+    socketio.run(app, port=3000, host="0.0.0.0", use_reloader=False, allow_unsafe_werkzeug=True, debug=True)
 
 if __name__ == '__main__':
     scheduler_thread = threading.Thread(target=run_scheduler)
