9e7708e15d18aff2b0182dbf87a61d70cac35241
thanos.batsiou <thanos.batsiou@gmail.com>
2023-11-24 13:02:45 +0200

    Changes on the messages save way

--- Twitch_Ctrl/client.py
+++ Twitch_Ctrl/client.py
@@ -4,9 +4,7 @@ from socketio.client import Client
 from dotenv import load_dotenv
 
 load_dotenv()
-
 socket = Client()
-messages = {}
 
 @socket.on('connect')
 def handle_connect():
@@ -15,20 +13,31 @@ def handle_connect():
 @socket.on('disconnect')
 def handle_disconnect():
     print('You disconnect from the server')
-    print('The total received messages are:', len(messages))
 
 @socket.on('message')
 def handle_message(data):
     print(data)
     streamer_name = data.get('streamer', 'unknown_streamer')
 
-    if streamer_name not in messages:
-        messages[streamer_name] = []
+    file_path = get_file_path(streamer_name)
+    streamer_messages = load_existing_messages(file_path)
+    streamer_messages.append(data)
 
-    messages[streamer_name].append(data)
-    
-    # Save messages to a JSON file
-    save_messages_to_file(streamer_name)
+    save_messages_to_file(file_path, streamer_messages)
+
+def load_existing_messages(file_path):
+    if os.path.exists(file_path):
+        with open(file_path, 'r', encoding='utf-8') as json_file:
+            return json.load(json_file)
+    else:
+        return []
+
+def get_file_path(streamer_name):
+    parent_folder = os.path.abspath(os.path.join(os.getcwd(), os.pardir))
+    messages_folder = os.path.join(parent_folder, 'data', 'messages')
+    os.makedirs(messages_folder, exist_ok=True)
+
+    return os.path.join(messages_folder, f'{streamer_name}_messages.json')
 
 def start_client():
     try:
@@ -37,16 +46,9 @@ def start_client():
     except Exception as e:
         print('> Error connecting: ', e)
 
-def save_messages_to_file(streamer_name):
-    parent_folder = os.path.abspath(os.path.join(os.getcwd(), os.pardir))  # Get the parent folder
-
-    messages_folder = os.path.join(parent_folder, 'data', 'messages')
-    os.makedirs(messages_folder, exist_ok=True)
-
-    file_path = os.path.join(messages_folder, f'{streamer_name}_messages.json')
-    
+def save_messages_to_file(file_path, streamer_messages):
     with open(file_path, 'w', encoding='utf-8') as json_file:
-        json.dump(messages[streamer_name], json_file, indent=4, ensure_ascii=False)
+        json.dump(streamer_messages, json_file, indent=4, ensure_ascii=False)
 
 if __name__ == '__main__': 
     start_client()
