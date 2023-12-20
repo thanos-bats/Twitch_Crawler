4377b20dfd5791ec137421c8511c27afe61eb191
thanos.batsiou <thanos.batsiou@gmail.com>
2023-12-20 16:46:27 +0200

    Sent kafka messages after documents, entity

--- Twitch_Ctrl/client.py
+++ Twitch_Ctrl/client.py
@@ -1,92 +1,159 @@
 import os
+import uuid
 import json
-from socketio.client import Client
-from dotenv import load_dotenv
+import datetime
 import requests
 
+from socketio.client import Client
+from dotenv import load_dotenv
+# import sys
+# sys.path.append(r'\Users\abatsioulas\Documents\Twitch_Crawler')
+# print(sys.path)
+from kafka.kafka_producer import MessageProducer
 
 load_dotenv()
-socket = Client()
 
-@socket.on('connect')
-def handle_connect():
-    print('Connection established')
+class SocketClient:
+    def __init__(self):
+        self.socket = Client()
+        self.producer = MessageProducer(os.getenv('BOOTSTRAP_SERVERS'))
 
-@socket.on('disconnect')
-def handle_disconnect():
-    print('You disconnect from the server')
+        self.socket.on('connect', self.handle_connect)
+        self.socket.on('disconnect', self.handle_disconnect)
+        self.socket.on('message', self.handle_message)
 
-@socket.on('message')
-def handle_message(data):
-    # data's structure { "data": {Message info}, "channels": {"streamer name": "jobId"} }
-    modified_message = data.get('data')
-    channels = data.get('channels')
-    streamer_name = modified_message.get('streamer', 'unknown_streamer')
+    def handle_connect(self):
+        print('Connection established')
 
-    document_data = {
-        "jobId": channels.get(streamer_name),
-        "domainId": f"twitch:comment:{channels.get(streamer_name)}",
-        "title": modified_message.get("message"),
-        "content": modified_message.get("message"),
-        "raw": "{}",
-        "source": "twitch",
-        "type": "twitch:comment",
-        "publishedAt": modified_message.get("created_at"),
-        "discoveredAt": modified_message.get("created_at"),
-        "lang": ""
-    }
-    entity_data = {
-        "domainId":f"twitch:profile:{channels.get(streamer_name)}",
-        "title":modified_message.get("username"),
-        "name":modified_message.get("username"),
-        "source":"twitch",
-        "type":"twitch:profile",
-        "discoveredAt": modified_message.get("created_at")
-    }
-    print(document_data)
-    try:
-        create_document_response = requests.post(f"{os.getenv('NEO4J_URL')}/documents/SocialMedia", json=document_data)
-        create_entity_response = requests.post(f"{os.getenv('NEO4J_URL')}/entities", json=entity_data)
-        
-        create_document_response.raise_for_status()
-        create_entity_response.raise_for_status()
+    def handle_disconnect(self):
+        print('You disconnect from the server')
 
-        print("POST requests successful")
-    except requests.exceptions.RequestException as e:
-        print(f"Request failed: {e}")
-        print("Response Content:", e.response.content)
-    except Exception as e:
-        print(f"An unexpected error occurred: {e}")
-
-def load_existing_messages(file_path):
-    if os.path.exists(file_path):
+    def start(self):
         try:
-            with open(file_path, 'r', encoding='utf-8') as json_file:
-                return json.load(json_file)
-        except json.JSONDecodeError as e:
-            print(f"JSONDecodeError: {e}")
-            return None
-    else:
-        print(f"File not found: {file_path}")
-        return []
+            self.socket.connect(os.getenv('SOCKET_URL'))
+            self.socket.wait()
+        except Exception as e:
+            print('> Error connecting: ', e)
+    
+    def handle_message(self, data):
+        # data's structure { "data": {Message info}, "channels": {"streamer name": "jobId"} }
+        modified_message = data.get('data')
+        channels = data.get('channels')
+        streamer_name = modified_message.get('streamer', 'unknown_streamer')
 
-def get_file_path(streamer_name):
-    parent_folder = os.path.abspath(os.path.join(os.getcwd(), os.pardir))
-    messages_folder = os.path.join(parent_folder, 'data', 'messages')
-    os.makedirs(messages_folder, exist_ok=True)
+        document_data = {
+            "jobId": channels.get(streamer_name),
+            "domainId": f"twitch:comment:{channels.get(streamer_name)}",
+            "title": modified_message.get("message"),
+            "content": modified_message.get("message"),
+            "raw": "{}",
+            "source": "twitch",
+            "type": "twitch:comment",
+            "publishedAt": modified_message.get("created_at"),
+            "discoveredAt": modified_message.get("created_at"),
+            "lang": ""
+        }
+        entity_data = {
+            "domainId":f"twitch:profile:{channels.get(streamer_name)}",
+            "title":modified_message.get("username"),
+            "name":modified_message.get("username"),
+            "source":"twitch",
+            "type":"twitch:profile",
+            "discoveredAt": modified_message.get("created_at")
+        }
+        print(f"Document data (before post request)> {document_data}")
 
-    return os.path.join(messages_folder, f'{streamer_name}_messages.json')
+        try:
+            # create_document_response = requests.post(f"{os.getenv('NEO4J_URL')}/documents/SocialMedia", json=document_data)
+            # create_entity_response = requests.post(f"{os.getenv('NEO4J_URL')}/entities", json=entity_data)
+            create_document_response, document_response = make_request(f"{os.getenv('NEO4J_URL')}/documents/SocialMedia", None, None, document_data, "POST")
+            create_entity_response, entity_response  = make_request(f"{os.getenv('NEO4J_URL')}/entities", None, None, entity_data, "POST")
+           
+            # create_document_response.raise_for_status()
+            # create_entity_response.raise_for_status()
 
-def start_client():
+            print("!!!POST requests successful!!")
+            print(f'>the created document is {create_document_response}, and response {document_response}')
+            print(f'>the created entity is {create_entity_response}, and response {entity_response}')
+            print(f">Now we will send the meesage to kafka\n")
+            topic, message = self.generate_message_tas_results(
+                datetime.datetime.utcnow().isoformat().split(".")[0] + 'Z',
+                streamer_name,
+                "test_caseId", 
+                "test_taskId", 
+                channels.get(streamer_name), 
+                create_document_response['data']['id'])
+            self.producer.send_message(topic, message)
+        except requests.exceptions.RequestException as e:
+            print(f"Request failed: {e}")
+            print("Response Content:", e)
+        except Exception as e:
+            print(f"An unexpected error occurred: {e}")
+
+    def generate_message_tas_results(self, sentUtc, streamer, caseId, taskId, jobId, documentId):
+        msg = {
+            "header": {
+                "topicName":os.getenv("TOPIC_MESSAGE_DONE"),
+                "msgId": documentId,
+                "sender": "CERTH",
+                "source": "Twitch",
+                "sentUtc": sentUtc
+            },
+            "body": {
+                "description": f"twitch chat logs on user's {streamer} stream",
+                "data": [
+                    {
+                        "id": documentId,
+                        "caseId": caseId,
+                        "taskId": taskId,
+                        "jobId": jobId
+                    }
+                ]
+            }
+        }
+
+
+        return os.getenv("TOPIC_MESSAGE_DONE"), json.dumps(msg)
+
+def make_request(url, params=None, headers=None, data=None, method="GET"):
     try:
-        socket.connect(os.getenv('SOCKET_URL'))
-        socket.wait()
-    except Exception as e:
-        print('> Error connecting: ', e)
+        if method.upper() == 'GET':
+            response = requests.get(url, params=params, headers=headers)
+        elif method.upper() == 'POST':
+            response = requests.post(url, json=data, headers=headers)
+        elif method.upper() == 'PATCH':
+            response = requests.patch(url, json=data, headers=headers)
+        else:
+            # Add more cases for other HTTP methods as needed
+            raise ValueError(f"Unsupported HTTP method: {method}")
+        response.raise_for_status()
+        response_data = {
+        'status': 'Success',
+        'data': response.json()  # Assuming the response contains JSON data
+        }
+        return response_data, 200
+    
+    except requests.exceptions.RequestException as e:
+        if hasattr(e, 'response') and e.response is not None:
+            error_data = e.response.json()
+            status_code = e.response.status_code
+        else:
+            error_data = {
+                'status': 'Error',
+                'message': str(e)
+            }
+            status_code = 500
 
-def save_messages_to_file(file_path, streamer_messages):
-    with open(file_path, 'w', encoding='utf-8') as json_file:
-        json.dump(streamer_messages, json_file, indent=4, ensure_ascii=False)
+        return error_data, status_code
+    
+    except ValueError as e:
+        error_data = {
+            'status': 'Error',
+            'message': 'JSON Decoding Error'
+            }
+        return error_data, 500
+    
+if __name__ == '__main__':
+    socket_client = SocketClient()
+    socket_client.start()
 
-if __name__ == '__main__': 
-    start_client()
