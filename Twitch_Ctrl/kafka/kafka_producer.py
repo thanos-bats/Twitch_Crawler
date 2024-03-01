dfc06bc9b38d465963b74f122e6167c25a074052
thanos.batsiou <thanos.batsiou@gmail.com>
2024-03-01 12:45:28 +0200

    Change kafka producer

--- Twitch_Ctrl/kafka/kafka_producer.py
+++ Twitch_Ctrl/kafka/kafka_producer.py
@@ -1,10 +1,19 @@
 from confluent_kafka import Producer
 import json
+import os
+from dotenv import load_dotenv
+load_dotenv()
 
-class MessageProducer:
-    def __init__(self, bootstrap_servers):
-        self.bootstrap_servers = bootstrap_servers
-        self.producer = Producer({'bootstrap.servers': bootstrap_servers})
+class ProducerHandler:
+    def __init__(self, bootstrap_servers=None):
+        if bootstrap_servers is None:
+            self.bootstrap_servers = os.getenv('BOOTSTRAP_SERVERS')
+        else:
+            self.bootstrap_servers = bootstrap_servers
+
+        print(f'Producer Configuration: {self.bootstrap_servers}')
+        conf = {'bootstrap.servers': self.bootstrap_servers}
+        self.producer = Producer(conf)
 
     def delivery_report(self, err, msg):
         if err is not None:
@@ -13,18 +22,32 @@ class MessageProducer:
             print('Message delivered to {} [{}]'.format(msg.topic(), msg.partition()))
 
     def send_message(self, topic, message_data):
-        # Convert the message to JSON
-        message_json = json.dumps(message_data)
+        print(f'I m sending message into {topic} topic')
 
         # Send the JSON message to Kafka
-        self.producer.produce(topic, value=message_json, callback=self.delivery_report)
+        json_message = json.dumps(message_data)
+        self.producer.produce(topic, json_message.encode('utf-8'), callback=self.delivery_report)
         self.producer.flush()
 
-    def close_producer(self):
-        self.producer.flush()
+    def close(self):
+        if self.producer is not None:
+            # Flush all outstanding messages before closing the producer
+            self.producer.flush()
 
 # Example usage:
-# producer = MessageProducer('your_kafka_bootstrap_servers')
+# bootstrap_servers = os.getenv('BOOTSTRAP_SERVERS')
+# topic = os.getenv('TOPIC_MESSAGE_DONE')
+
+# print('Bootstrap Servers:', bootstrap_servers)
+# print('Topic:', topic)
+
+# # Instantiate the MessageProducer
+# producer = MessageProducer(bootstrap_servers)
+
+
+# # Send a message
 # message_data = {'key1': 'value1', 'key2': 'value2'}
-# producer.send_message('your_kafka_topic', message_data)
+# producer.send_message(topic, message_data)
+
+# # Close the producer
 # producer.close_producer()
