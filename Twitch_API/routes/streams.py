53207a95b3b46a959b2b7c4f277d008492b2deab
thanos.batsiou <thanos.batsiou@gmail.com>
2024-07-03 16:22:22 +0300

    Initial commit for second creation Task way at background

--- Twitch_API/routes/streams.py
+++ Twitch_API/routes/streams.py
@@ -1,8 +1,9 @@
 # routes/streams.py
 from flask import jsonify, request
-from services.streams import get_streams, retrieve_comments_start, retrieve_comments_stop, create_jobs_per_streamer, get_all_tags, get_streams_by_tags
+
+from services.streams import get_streams, retrieve_comments_start, retrieve_comments_stop, create_jobs_per_streamer, get_all_tags, get_streams_by_tags, evaluate_expression, create_task_background
 from routes import streams_bp
-from utilities.utils import get_error_message
+from utilities.utils import get_error_message, parse_query
 
 # Returns a list of live streams for a specific game id or a user id
 @streams_bp.route('/', methods=['GET'])
@@ -20,7 +21,7 @@ def get_game_streams():
     if not game_ids and not user_id and not language and not user_login:
         return get_error_message('game_id, user_id, user_login or language')
 
-    data, status_code = get_streams(game_ids, number_of_results, user_id, user_login, language,cursor, request.path)
+    data, status_code = get_streams(game_ids, number_of_results, user_id, user_login, language, cursor, request.path)
     return jsonify(data), status_code
 
 # Starts retrieving comments for a list of live streams
@@ -67,12 +68,44 @@ def get_all_tags_route():
 @streams_bp.route('/tags/search', methods=['GET'])
 def get_streams_by_tags_route():
     tags_param = request.args.get('tags', None)
+    tokens = parse_query(tags_param)
+    print(f"Tokens: {tokens}")
+
     crawl_id = request.args.get('crawl_id')
     game_ids_param = request.args.get('game_ids')
     if (not tags_param) or (not crawl_id):
         return get_error_message("tags and the crawl_id")
     
-    tags = [str(tag) for tag in tags_param.split(',')]
     game_ids = [int(game_id) for game_id in game_ids_param.split(',')] if game_ids_param else []
-    data, status_code = get_streams_by_tags(tags, crawl_id, game_ids)
-    return jsonify(data), status_code
+    resp = []
+    for game_id in game_ids:
+        
+        tokens_copy = tokens[:]
+        print(f"\n\nGAME ID: {game_id}\n-----------------------\nTokens: {tokens_copy}")
+        data, status_code = evaluate_expression(tokens_copy, crawl_id, game_id)
+        if status_code != 200: return data, status_code
+        resp.extend(data)
+
+    print(f"The total length is {len(resp)}")
+    return jsonify({"data": resp}), status_code
+
+@streams_bp.route('/comments/background', methods=['POST'])
+def create_task_background_route():
+    data = request.get_json()
+    game_ids_str = data.get('game_id')
+    game_ids = [int(game_id) for game_id in game_ids_str.split(',')] if game_ids_str else []
+
+    languages_str = data.get('language', None)
+    languages = [str(lang) for lang in languages_str.split(',')] if languages_str else []
+
+    tags_str = data.get('tags', None)
+    tags = [str(tag) for tag in tags_str.split(',')] if tags_str else []
+
+    period = data.get('period', 6)
+    
+    # Here I want to call the create_task_background every 'period' hours
+    # Is an API so I want to run at background and continue the code
+
+    resp = "OK"
+    status_code = 200
+    return jsonify({"data": resp}), status_code
